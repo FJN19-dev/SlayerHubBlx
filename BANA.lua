824a66be91c2754a79cd7ee440969f3bff626218
FJN19-dev <josealbertsouzasantos189@gmail.com>
2025-12-16 21:04:50 -0300

    Update BANA.lua

--- BANA.lua
+++ BANA.lua
@@ -4885,6 +4885,50 @@ spawn(function()
     end
 end)
 end
+
+if World2 or World3 then
+local Toggle1 = Sub:AddToggle({
+  Name = "Auto Comprar Haki Color",
+  Description = "",
+  Default = false 
+})
+Toggle1:Callback(function(Value)
+    getgenv().Auto_Buy_Enchancement = Value 
+end)
+spawn(function()
+    local lastCallTime = 0
+    while true do
+        if getgenv().Auto_Buy_Enchancement then
+            local currentTime = tick()
+            if currentTime - lastCallTime >= 2 then
+                lastCallTime = currentTime
+                local args = {
+                    [1] = "ColorsDealer",
+                    [2] = "2"
+                }
+                pcall(function()
+                    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
+                end)
+            end
+        end
+        wait(0.1)
+    end
+end)
+end
+
+local isHopping = false
+if World2 or World3 then
+local Toggle1 = Sub:AddToggle({
+  Name = "Hop Server [ Haki Cor Ou Esapada Lendaria]",
+  Description = "",
+  Default = false 
+})
+Toggle1:Callback(function(Value)
+    isHopping = state
+    if isHopping then
+        Hop()
+    end
+end)
 ----- Quest ------
 
 
